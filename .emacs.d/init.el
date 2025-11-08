15be78f11be0dd4817a54f60e5148e82a30408cc
Anthony Lamorte <lamorte@gmail.com>
2025-11-08 10:33:47 -0500

    fix screen jitter when company-mode kicks in

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -352,13 +352,17 @@
 (use-package company
   :after lsp-mode
   :hook (lsp-mode . company-mode)
-  :bind (:map company-active-map
-	      ("<tab>" . company-complete-selection))
-        (:map lsp-mode-map
-	      ("<tab>" . company-indent-or-complete-common))
+  :bind
+  (:map company-active-map
+  	("<tab>" . company-complete-selection))
+  (:map lsp-mode-map
+  	("<tab>" . company-indent-or-complete-common))
   :custom
   (company-minimum-prefix-length 1)
-  (company-idle-delay 0.0))
+  (company-idle-delay 0.0)
+  ;; Disable company-echo-metadata-frontend because the echo area refresh was jittery
+  (company-frontends '(company-pseudo-tooltip-unless-just-one-frontend
+		       company-preview-frontend)))
 
 (use-package company-box
   :hook (company-mode . company-box-mode))
