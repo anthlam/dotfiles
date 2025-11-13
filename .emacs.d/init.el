926d3924fd5b98ce254d8b8ac1e3ecb49f8b0bb4
Anthony Lamorte <lamorte@gmail.com>
2025-11-12 21:13:04 -0500

    setup automatic package updates

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -31,6 +31,15 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+(use-package auto-package-update
+  :custom
+  (auto-package-update-interval 7)
+  (auto-package-update-prompt-before-update t)
+  (auto-package-update-hide-results t)
+  :config
+  (auto-package-update-maybe)
+  (auto-package-update-at-time "09:00"))
+
 ;; ===== KEYBINDINGS =====
 
 ;; Key-binding to open init.el
